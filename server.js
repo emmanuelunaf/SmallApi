28b993e6bbbc4e94e14a5ff5f79f10772483514c
Emmanuel Luna Felix <emmanuel.lunaf@softtek.com>
2017-10-16 13:03:13 -0500

    *First commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 80,
   mongoose = require('mongoose'),
   Task = require('./api/models/smallApiModel'), //created model loading here
   bodyParser = require('body-parser');
